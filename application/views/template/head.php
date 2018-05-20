e0a778d64df3bdcb7b005e1c8f87439aa3596e01
josemrp <josemanuelromerop@gmail.com>
2018-05-20 10:14:47 -0400

    Incluir font awesome, fixed-top menu y arreglos generales del menu

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
-        <title>Index</title>
+        <title>Candy UCAB</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv=”Pragma” content=”no-cache”>
@@ -10,10 +9,13 @@
         <meta http-equiv=”Last-Modified” content=”0″>
         <meta http-equiv=”Cache-Control” content=”no-cache, mustrevalidate”>
         <meta http-equiv=”Pragma” content=”no-cache”>
+        <!-- CSS -->
         <link rel="stylesheet" href="<?php echo base_url() ?>public/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?php echo base_url() ?>public/css/micss.css" type="text/css" media="all"> 
+        <link href="<?php echo base_url() ?>public/css/fontawesome-all.min.css" rel="stylesheet">
+        <!-- JS -->
         <script src="<?php echo base_url() ?>public/js/jquery-3.3.1.min.js"></script>
         <script src="<?php echo base_url() ?>public/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-        <link rel="stylesheet" href="public/css/micss.css" type="text/css" media="all"> 
     </head>
     <body>
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,46 +1,63 @@
-<nav class="navbar navbar-expand-md ">
+<nav class="navbar fixed-top navbar-expand-md bg-white">
+    <!-- Left section -->
     <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
         <ul class="navbar-nav mr-auto ">
-            <li class="nav-item active  border boder-dark">
+            <li class="nav-item active text-center">
                 <a class="nav-link" href="#">
-                    <img src="public/img/usuario.jpg" width="60" height="60" alt="usuario">
+                    <!-- <img src="public/img/usuario.jpg" width="60" height="60" alt="usuario"> -->
+                    <i class="fas fa-user fa-2x text-dark"></i> <br>
+                    <small>Iniciar Sesión</small>
                 </a>
             </li>
             
-            <li class="nav-item  border boder-dark">
-                 <img src="public/img/localizar.jpg" width="60" height="60" alt="localizar">
+            <li class="nav-item text-center">
+                    <!-- <img src="public/img/localizar.jpg" width="60" height="60" alt="localizar"> -->
+                <a class="nav-link" href="#">
+                    <i class="fas fa-map-marker-alt fa-2x text-dark"></i> <br>
+                    <small>Encuentranos</small>
+                </a>
             </li>
             
         </ul>
     </div>
+    <!-- Colapse section -->
     <div class="mx-auto order-0 ">
-        <a class="navbar-brand" href="#" >
-            <img src="public/img/logo.png" width="550" height="95" alt="CandyUcab"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-            <span class="navbar-toggler-icon"></span>
+            <i class="fas fa-align-justify fa-2x text-dark"></i>
         </button>
+        <a class="navbar-brand" href="<?php echo base_url() ?>" >
+            <img src="<?php echo base_url() ?>public/img/logo.png" width="300" alt="CandyUcab">
+        </a>
     </div>
+    <!-- Right section -->
     <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
         
         <ul class="navbar-nav ml-auto ">
-            <li class="nav-item border boder-dark">
-                <a class="nav-link " href="#">
-                    <img src="public/img/telefono.jpg " width="60" height="60" alt="telefono">
-                </a>
-            </li>
-            <li class="nav-item border boder-dark" >
+            <li class="nav-item text-center">
+                    <!-- <img src="public/img/telefono.jpg " width="60" height="60" alt="telefono"> -->
                 <a class="nav-link" href="#">
-                    <img src="public/img/sig.jpg"   width="60" height="60" alt="Sign in">
+                    <i class="fas fa-phone-volume fa-2x text-dark"></i> <br>
+                    <small>Contactanos</small>
                 </a>
             </li>
-            <li class="nav-item border boder-dark">
+            <!-- <li class="nav-item" >
+                <a class="nav-link" href="#">
+                    <!- - <img src="public/img/sig.jpg"   width="60" height="60" alt="Sign in"> - ->
+                    <i class="fas fa-map-marker-alt fa-2x text-dark"></i> <br>
+                    <small>Sig</small>
+                </a>
+            </li> -->
+            <li class="nav-item text-center">
                 <a class="nav-link " href="#">
-                    <img src="public/img/carrito.png " width="60" height="60" alt="carrito">
+                    <!-- <img src="public/img/carrito.png " width="60" height="60" alt="carrito"> -->
+                    <i class="fas fa-shopping-cart fa-2x text-dark"></i> <br>
+                    <small>Ver carrito</small>
                 </a>
             </li>
-            
         </ul>
-        
     </div>
-
 </nav>
+<!-- make some space after the navbar -->
+<br>
+<br>
+<br>
