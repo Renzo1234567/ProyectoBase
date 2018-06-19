7b8c330ca8b37fd368051d703c6739a59f4d880a
josemrp <josemanuelromerop@gmail.com>
2018-06-19 00:23:51 -0400

    Modelo base para las conexiones con base de datos

---
+++ application/core/MY_Model.php
@@ -0,0 +1,18 @@
+<?php
+
+class MY_Model extends CI_Model
+{
+
+    protected $conn;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1234");
+        if (!$this->conn) {
+            echo "Ocurrió un error de conexion con la base de datos.\n";
+            exit;
+        }
+    }
+
+}
