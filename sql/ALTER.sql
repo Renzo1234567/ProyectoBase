005941fdac55e0aa822935edf061272265d9b80f
josemrp <josemanuelromerop@gmail.com>
2018-06-18 23:48:02 -0400

    SQL by @Jaramillo

--- sql/ALTER.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE Natural_BD ADD constraint cp_natu_rif primary key (natu_rif);
-ALTER TABLE Natural_BD ADD constraint cf_natu_lugar foreign key (cf_natu_lugar);
-ALTER TABLE Natural_BD ADD constraint cf_natu_usuario foreign key (cf_natu_usuario);
-ALTER TABLE Natural_BD ADD constraint cf_natu_punto foreign key (cf_natu_punto);
-
