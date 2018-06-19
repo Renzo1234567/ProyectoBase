32a75d3d7a102fc783d1307d9b612e808f6ab0bb
UCAB-Jaramillo <33133823+UCAB-Jaramillo@users.noreply.github.com>
2018-06-19 14:19:52 -0400

    Update Drop.sql

--- sql/Drop.sql
+++ sql/Drop.sql
@@ -1,38 +1,39 @@
-drop table [ IF EXISTS ] lugar_bd
-drop table [ IF EXISTS ] tienda_bd 	
-drop table [ IF EXISTS ] rol_bd
-drop table [ IF EXISTS ] permiso_rol 
-drop table [ IF EXISTS ] tipo_bd
-drop table [ IF EXISTS ] producto_tipo
-drop table [ IF EXISTS ] producto_bd
-drop table [ IF EXISTS ] diario_bd
-drop table [ IF EXISTS ] oferta_bd
-drop table [ IF EXISTS ] natural_bd
-drop table [ IF EXISTS ] juridico_bd
-drop table [ IF EXISTS ] tienda_cliente
-drop table [ IF EXISTS ] carnet_bd
-drop table [ IF EXISTS ] empleado_bd
-drop table [ IF EXISTS ] usuario_bd
-drop table [ IF EXISTS ] vacacion_bd
-drop table [ IF EXISTS ] asistencia_bd 
-drop table [ IF EXISTS ] horario_bd
-drop table [ IF EXISTS ] empleado_horario
-drop table [ IF EXISTS ] departamento_bd 
-drop table [ IF EXISTS ] telefono_bd
-drop table [ IF EXISTS ] banco_bd
-drop table [ IF EXISTS ] punto_bd
-drop table [ IF EXISTS ] cliente_punto
-drop table [ IF EXISTS ] presupuesto_bd
-drop table [ IF EXISTS ] tarjeta_bd
-drop table [ IF EXISTS ] cheque_bd
-drop table [ IF EXISTS ] efectivo_bd
-drop table [ IF EXISTS ] estatus_bd
-drop table [ IF EXISTS ] compra_bd
-drop table [ IF EXISTS ] compra_estatus
-drop table [ IF EXISTS ] presupuesto_producto_bd
-drop table [ IF EXISTS ] pago_bd
-drop table [ IF EXISTS ] ordenreposicion_bd
-drop table [ IF EXISTS ] orden_producto
-drop table [ IF EXISTS ] mediospago
-drop table [ IF EXISTS ] inventario
+drop table IF EXISTS  lugar_bd
+drop table IF EXISTS  tienda_bd 	
+drop table IF EXISTS  rol_bd
+drop table IF EXISTS  permiso_rol 
+drop table IF EXISTS  tipo_bd
+drop table IF EXISTS  producto_tipo
+drop table IF EXISTS  producto_bd
+drop table IF EXISTS  diario_bd
+drop table IF EXISTS  oferta_bd
+drop table IF EXISTS  natural_bd
+drop table IF EXISTS  juridico_bd
+drop table IF EXISTS  tienda_cliente
+drop table IF EXISTS  carnet_bd
+drop table IF EXISTS  empleado_bd
+drop table IF EXISTS  usuario_bd
+drop table IF EXISTS  vacacion_bd
+drop table IF EXISTS  asistencia_bd 
+drop table IF EXISTS  horario_bd
+drop table IF EXISTS  empleado_horario
+drop table IF EXISTS  departamento_bd 
+drop table IF EXISTS  telefono_bd
+drop table IF EXISTS  banco_bd
+drop table IF EXISTS  punto_bd
+drop table IF EXISTS  cliente_punto
+drop table IF EXISTS  presupuesto_bd
+drop table IF EXISTS  tarjeta_bd
+drop table IF EXISTS  cheque_bd
+drop table IF EXISTS  efectivo_bd
+drop table IF EXISTS  estatus_bd
+drop table IF EXISTS  compra_bd
+drop table IF EXISTS  compra_estatus
+drop table IF EXISTS  presupuesto_producto_bd
+drop table IF EXISTS  pago_bd
+drop table IF EXISTS  ordenreposicion_bd
+drop table IF EXISTS  orden_producto
+drop table IF EXISTS  mediospago
+drop table IF EXISTS  inventario
+
 
